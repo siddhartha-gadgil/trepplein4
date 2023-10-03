70431175b5f0908916cf170c952551ac62cf78bc
Siddhartha Gadgil <siddhartha.gadgil@gmail.com>
2023-10-03 12:58:26 +0530

    Names documented and refactored

---
+++ src/main/scala/trepplein/package.scala
@@ -0,0 +1,12 @@
+/**
+  * 
+  * Lean is an interactive theorem prover based on dependent type theory.  For
+  * additional trust, Lean can export the generated proofs so that they can be
+  * independently verified (see https://github.com/leanprover/lean/blob/master/doc/export_format.md).    * 
+  * Trepplein is a tool that can check these exported proofs.
+  * 
+  */
+
+package object trepplein {
+  
+}
